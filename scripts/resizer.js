624ed34ea9f8f9913ebe3c3f434173c7f4514a5e
donofanne <donofanne@users.noreply.github.com>
2015-12-04 21:54:40 -0600

    fixed small-->big-->small sizing issues for the schedules

--- scripts/resizer.js
+++ scripts/resizer.js
@@ -17,7 +17,7 @@ function resizeContainers()
                     {
                         // set it back to the original height, then get that height
                         // (yes, I know it looks stupid)
-                        maxHeight = Math.max( maxHeight, $(this).outerHeight( 'initial' ).outerHeight() );
+                        maxHeight = Math.max( maxHeight, $( this ).outerHeight( 'initial' ).outerHeight() );
                     }
                 );
                 $(this).children( 'div.content' ).each(
@@ -34,6 +34,12 @@ function resizeContainers()
                     {
                         var height = $( this ).outerHeight( 'initial' ).outerHeight(); // gets the outer height after setting it to the original height
                         $( this ).outerHeight( height + 25 ); // adds 25 for spacing
+                    }
+                );
+                $( this ).children( 'div.content.schedule' ).each(
+                    function( i )
+                    {
+                        $( this ).css( "height", "" );
                     }
                 );
             }
